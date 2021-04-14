7a7b4e6e78225bab148773e8672f7b26f76d2e21
vadim <79169082210@mail.ru>
2021-04-14 12:08:40 +0300

    add function "drag-and-drop"

--- runner.js
+++ runner.js
@@ -1,10 +1,18 @@
-const knopka = document.querySelector('.plus');        // получил кнопку
-const textarea = document.querySelector('.textarea'); // и текстовое поле
+const knopkaAddElement = document.querySelector('.plus');       // получил кнопку "ПЛЮС"
+const textarea = document.querySelector('.textarea');          // и текстовое поле  
 
-knopka.addEventListener('click', (event) => {
+knopkaAddElement.addEventListener('click', (event) => {
     let newLi = document.createElement('li');            // создаю лишку и добавляю начинку
-    newLi.innerHTML = `<form class="formText"><textarea class="textarea" rows="1" cols="20"></textarea><button class="reset" type="button">x</button></form>`;
+    newLi.innerHTML = `<form class="formText tasks__item"><textarea class="textarea" rows="1" cols="20"></textarea><button class="reset" type="button">x</button></form>`;
     document.querySelector('.textareaUl').append(newLi);
+    deleteElementOfList();                              // сразу после создания элемента списка запускаю к нему функцию удаления
+});
+
+const buttonAddElement = document.querySelector('.button');        // получил кнопку
+buttonAddElement.addEventListener('click', (event) => {
+    let newLi2 = document.createElement('li');            // создаю лишку и добавляю начинку
+    newLi2.innerHTML = `<form class="formText tasks__item"><textarea class="textarea" rows="1" cols="20"></textarea><button class="reset" type="button">x</button></form>`;
+    document.querySelector('.textareaUl').append(newLi2);
     deleteElementOfList();                              // сразу после создания элемента списка запускаю к нему функцию удаления
 });
 //-------------------------------------------------------------------------------------------------------------//
@@ -63,4 +71,68 @@ buttonSort.addEventListener('click', (event) => {       // обработчик 
     }
 });
 //-------------------------------------------------------------------------------------------------------------//
+//         Реализую возможность перетаскивания элементов списка. 
 
+const tasksListElement = document.querySelector(`.textareaUl`);
+const taskElements = tasksListElement.querySelectorAll(`.tasks__item`);  // <-- добавил класс
+
+//       Перебираю все элементы списка и присваиваю нужное значение
+for (const task of taskElements) {
+    task.draggable = true;
+}
+//-------------------------------------------------------------------------------------------//
+//      Добавляю реакцию на начало и конец перетаскивания через добавение класса
+tasksListElement.addEventListener(`dragstart`, (evt) => {             // <-- добавил класс в css   
+    evt.target.classList.add(`selected`);                 // + отразил его в функции добавления элемента
+})
+  
+tasksListElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+});
+//-------------------------------------------------------------------------------------------//
+//      Реализую логику перетаскивания элементов + учёл положение курсора относительно центра элемента
+
+const getNextElement = (cursorPosition, currentElement) => {
+    // Получаю объект с размерами и координатами
+    const currentElementCoord = currentElement.getBoundingClientRect();
+    // Нахожу вертикальную координату центра текущего элемента
+    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+    // Если курсор выше центра элемента, возвращаю текущий элемент
+    // В ином случае — следующий DOM-элемент
+    const nextElement = (cursorPosition < currentElementCenter) ?
+        currentElement :
+        currentElement.nextElementSibling;
+    return nextElement;
+};
+
+tasksListElement.addEventListener(`dragover`, (evt) => {
+    // Разрешаю сбрасывать элементы в эту область
+    evt.preventDefault();
+    // Нахожу перемещаемый элемент
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    // Нахожу элемент, над которым в данный момент находится курсор
+    const currentElement = evt.target;
+    // Проверяю, что событие сработало:
+    // 1. не на том элементе, который мы перемещаем,
+    // 2. именно на элементе списка
+    const isMoveable = activeElement !== currentElement &&
+      currentElement.classList.contains(`tasks__item`);
+    // Если нет, прерываю выполнение функции
+    if (!isMoveable) {
+      return;
+    }
+    // evt.clientY — вертикальная координата курсора в момент,
+    // когда сработало событие
+    const nextElement = getNextElement(evt.clientY, currentElement);
+    // Проверяю, нужно ли менять элементы местами
+    if (
+      nextElement && 
+      activeElement === nextElement.previousElementSibling ||
+      activeElement === nextElement
+    ) {
+      // Если нет, выхожу из функции, чтобы избежать лишних изменений в DOM
+      return;
+    }
+    tasksListElement.insertBefore(activeElement, nextElement);
+});
+//-------------------------------------------------------------------------------------------//
